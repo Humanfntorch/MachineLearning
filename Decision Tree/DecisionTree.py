e4ee47f3d514fc4701de890f7bd3b7e7dfb564b2
Jake <57159859+Humanfntorch@users.noreply.github.com>
2022-11-05 22:32:59 -0600

    Update DecisionTree.py

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -82,11 +82,34 @@ def gain(df, lbl, FLAG):
                 
         attWeightedGainDict[attName] = Hset - H
     return attWeightedGainDict
-def find_depth(dictionary):
-    return sum([find_depth(depth) if isinstance(depth, dict) else 0 for depth in dictionary.values()])
 
-import time
-def _ID3(dtree, df, lbl, treeDepth, maxDepth, FLAG, numChildren, maxChildren, randForest):
+
+def find_depth(dictionary):
+    #return sum([find_depth(depth) if isinstance(depth, dict) else 0 for depth in dictionary.values()])
+    
+    # Holds depth of each branch in dict
+    branchDepths = [] 
+    # Used to flag a new branch, which requires +2 to be added to length
+    isNewBranch = False
+    # Count of all opening braces in a given branch.
+    braceCount = 0
+    dictToString = str(dictionary)
+    for c in dictToString:
+        if(c == '{'):
+            if(isNewBranch):
+                braceCount = 2
+                isNewBranch = False
+            braceCount += 1
+        elif(c == '}' and not isNewBranch):
+            branchDepths.append(braceCount)
+            isNewBranch = True
+    branchDepths = np.asarray(branchDepths)
+    # Each new depth in tree corresponds to two {
+    branchDepths = np.ceil(np.divide(branchDepths, 2))
+    return np.max(branchDepths)
+    
+
+def _ID3(dtree, df, lbl, stableDf, treeDepth, maxDepth, FLAG, numChildren, maxChildren, randForest):
     
     if((treeDepth >= maxDepth and numChildren == 0)):
         return 
@@ -100,7 +123,7 @@ def _ID3(dtree, df, lbl, treeDepth, maxDepth, FLAG, numChildren, maxChildren, ra
         key = list(dtree.keys())[0]
         dtree[key] = lbl.value_counts().idxmax()
         return
-    elif(len(df) == 0):
+    elif(len(df) == 0 or len(df.columns) - 1 == 0):
         return
 
     nodeBool = numChildren < maxChildren
@@ -117,11 +140,11 @@ def _ID3(dtree, df, lbl, treeDepth, maxDepth, FLAG, numChildren, maxChildren, ra
         lblTemp = dfTemp[lbl.name]
         gainDict = gain(dfTemp, lblTemp, FLAG)
         split_att = max(gainDict, key = gainDict.get)
-        split_att_vals = df[split_att].unique()
+        split_att_vals = df[split_att].unique().tolist()
     else:    
         gainDict = gain(df, lbl, FLAG)
         split_att = max(gainDict, key = gainDict.get)
-        split_att_vals = df[split_att].unique()
+        split_att_vals = df[split_att].unique().tolist()
     
     if(not(maxChildren is np.inf)):
         split_att_vals = split_att_vals[0:maxChildren]
@@ -135,6 +158,12 @@ def _ID3(dtree, df, lbl, treeDepth, maxDepth, FLAG, numChildren, maxChildren, ra
     treeDepth += 1
     
     
+    # copy missing split_attribute valuesinto split_att list
+    stableSplit_att_vals = stableDf[split_att].unique().tolist()
+    if(len(stableSplit_att_vals) > len(split_att_vals)):
+        for att_val in stableSplit_att_vals:
+            if(att_val not in split_att_vals):
+                split_att_vals.append(att_val)
     
     # copy data in temp to manipulate recursively
     dfTemp = df
@@ -147,25 +176,28 @@ def _ID3(dtree, df, lbl, treeDepth, maxDepth, FLAG, numChildren, maxChildren, ra
             dfTemp = dfTemp.drop(labels = lblTemp.index.tolist(), axis = 0)
         # val is empty, add most common lbl
         elif(len(lblTemp.unique()) == 0 and nodeBool):
-            dtree[split_att][val] = max(lbl.value_counts().index.tolist())
+            dtree[split_att][val] = stableDf[lbl.name].value_counts().idxmax()
             numChildren += 1
-            dfTemp = dfTemp.drop(labels = lblTemp.index.tolist(), axis = 0)
+            # dfTemp = dfTemp.drop(labels = lblTemp.index.tolist(), axis = 0)
         else:
             if(nodeBool):
                 numChildren += 1
                 dfChildTemp = dfTemp[dfTemp[split_att] == val].drop(columns = [split_att])
                 childTree = {}
                 childTree[val] = {}
-                _ID3(childTree, dfChildTemp, dfChildTemp[lbl.name], treeDepth, maxDepth, FLAG, numChildren, maxChildren, randForest)
+                _ID3(childTree, dfChildTemp, dfChildTemp[lbl.name], stableDf, treeDepth, maxDepth, FLAG, numChildren, maxChildren, randForest)
                 dtree[split_att][val] = childTree[val]
-    
+                
     dfTemp = dfTemp.drop(columns = [split_att])
     lblTemp = dfTemp[lbl.name]
-    _ID3(dtree, dfTemp, lblTemp, treeDepth + 1, maxDepth, FLAG, 0, maxChildren, randForest)
+    tempTreeDepth = find_depth(dtree)
+    if(tempTreeDepth > treeDepth):
+        treeDepth = tempTreeDepth
+    _ID3(dtree, dfTemp, lblTemp, stableDf, treeDepth + 1, maxDepth, FLAG, 0, maxChildren, randForest)
 
 def decision_tree(data, label_name, maxDepth, splitAlgorithm, maxChildren:float = np.inf, randForest = None):
     dt = {}
-    _ID3(dt, data, data[label_name], 0, maxDepth, splitAlgorithm, 0, maxChildren, randForest)
+    _ID3(dt, data, data[label_name], data, 0, maxDepth, splitAlgorithm, 0, maxChildren, randForest)
     return dt
     
          
@@ -173,10 +205,11 @@ def classify(dtree, element):
     stopIter = False
     while(not stopIter):
         if not isinstance(dtree, dict):
-            return dtree
+            return dtree, None, None
         
         dtreeIter = iter(dtree)
         val = next(dtreeIter, None)
+        
         if(val == None):
             stopIter = True
         else:
@@ -184,30 +217,29 @@ def classify(dtree, element):
             if attVal in dtree[val]:
                 dtree = dtree[val][attVal]
             else: 
-                return None
-    return None
+                return None, val, attVal
+    return None, val, attVal
 
 def prediction(dtree, data, label_name):
+    pd.options.mode.chained_assignment = None  # default='warn'
     hitCnt = 0
     missCnt = 0
-    data['prediction'] = None
+    noneCount = 0
+    df2 = pd.DataFrame(index=data.index)
+    df2['prediction'] = None
     for idx, row in data.iterrows():
-        classification = classify(dtree, data.iloc[idx])
+        classification, val, attVal = classify(dtree, data.iloc[idx])
         if(classification is None):
-            data['prediction'].iloc[idx] = data[label_name].mean()
-            if(data[label_name].mean() == data[label_name].iloc[idx]):
-                hitCnt += 1
-            else:
-                missCnt += 1
-            continue
-        for el in classification:
-            if(el == data[label_name].iloc[idx]):
-                hitCnt += 1
-                data['prediction'].iloc[idx] = el
-            else:
-                missCnt += 1
-                data['prediction'].iloc[idx] = el
-    return data['prediction'], missCnt/(missCnt + hitCnt)
+            noneCount += 1
+        if(classification == data[label_name].iloc[idx]):
+            hitCnt += 1
+            df2['prediction'].iloc[idx] = classification
+        else:
+            missCnt += 1
+            df2['prediction'].iloc[idx] = data[label_name].iloc[idx]
+    if(len(df2['prediction'][df2['prediction'] == None]) > 0):
+        print("ERROR: Tree Construction Resulted in None Classification. Total None Values = ", noneCount)
+    return df2['prediction'], missCnt/(missCnt + hitCnt)
 
 def convert_numerical_binary(df, label_names):
     for label_name in label_names:
@@ -242,9 +274,9 @@ with open ("cardata-desc.txt", 'r') as f:
 dfCarsTest.columns = terms
 dfCarsTrain.columns = terms
 
-#carTree1 = decision_tree(dfCarsTrain, "label", 1, ENTROPYFLAG)
-carTree2 = decision_tree(dfCarsTrain, "label", 2, ENTROPYFLAG)
-carTree3 = decision_tree(dfCarsTrain, "label", 3, ENTROPYFLAG)
+# carTree1 = decision_tree(dfCarsTrain, "label", 1, ENTROPYFLAG)
+# carTree2 = decision_tree(dfCarsTrain, "label", 2, ENTROPYFLAG)
+# carTree3 = decision_tree(dfCarsTrain, "label", 3, ENTROPYFLAG)
 # carTree4 = decision_tree(dfCarsTrain, "label", 4, ENTROPYFLAG)
 # carTree5 = decision_tree(dfCarsTrain, "label", 5, ENTROPYFLAG)
 # carTree6 = decision_tree(dfCarsTrain, "label", 6, ENTROPYFLAG)
@@ -259,17 +291,17 @@ carTree3 = decision_tree(dfCarsTrain, "label", 3, ENTROPYFLAG)
 
 # print(peAvg)
 
-#dfBankTest = pd.read_csv('test.csv')
-# dfBankTrain = pd.read_csv('train.csv')
-# cols = ['age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'day', 'month', 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'y']
-# dfBankTest.columns = cols
-# dfBankTrain.columns = cols
-# numeric_atts = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
-# unknown_atts = ['job', 'education', 'contact', 'poutcome']
-# convert_numerical_binary(dfBankTest, numeric_atts)
-# convert_numerical_binary(dfBankTrain, numeric_atts)
-# convert_unknown(dfBankTest, unknown_atts)
-# convert_unknown(dfBankTrain, unknown_atts)
+dfBankTest = pd.read_csv('test.csv')
+dfBankTrain = pd.read_csv('train.csv')
+cols = ['age', 'job', 'marital', 'education', 'default', 'balance', 'housing', 'loan', 'contact', 'day', 'month', 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'y']
+dfBankTest.columns = cols
+dfBankTrain.columns = cols
+numeric_atts = ['age', 'balance', 'day', 'duration', 'campaign', 'pdays', 'previous']
+unknown_atts = ['job', 'education', 'contact', 'poutcome']
+convert_numerical_binary(dfBankTest, numeric_atts)
+convert_numerical_binary(dfBankTrain, numeric_atts)
+convert_unknown(dfBankTest, unknown_atts)
+convert_unknown(dfBankTrain, unknown_atts)
 # bT1 = decision_tree(dfBankTrain, "y", 1, GINIFLAG)
 # bT2 = decision_tree(dfBankTrain, "y", 2, GINIFLAG)
 # bT3 = decision_tree(dfBankTrain, "y", 3, GINIFLAG)
@@ -287,27 +319,25 @@ carTree3 = decision_tree(dfCarsTrain, "label", 3, ENTROPYFLAG)
 # bT15 = decision_tree(dfBankTrain, "y", 15, GINIFLAG)
 # bT16 = decision_tree(dfBankTrain, "y", 16, GINIFLAG)
 
-# be1 = prediction_error(bT1, dfBankTrain, 'y')
-# be2 = prediction_error(bT2, dfBankTrain, 'y')
-# be3 = prediction_error(bT3, dfBankTrain, 'y')
-# be4 = prediction_error(bT4, dfBankTrain, 'y')
-# be5 = prediction_error(bT5, dfBankTrain, 'y')
-# be6 = prediction_error(bT6, dfBankTrain, 'y')
-# be7 = prediction_error(bT7, dfBankTrain, 'y')
-# be8 = prediction_error(bT8, dfBankTrain, 'y')
-# be9 = prediction_error(bT9, dfBankTrain, 'y')
-# be10 = prediction_error(bT10, dfBankTrain, 'y')
-# be11 = prediction_error(bT11, dfBankTrain, 'y')
-# be12 = prediction_error(bT12, dfBankTrain, 'y')
-# be13 = prediction_error(bT13, dfBankTrain, 'y')
-# be14 = prediction_error(bT14, dfBankTrain, 'y')
-# be15 = prediction_error(bT15, dfBankTrain, 'y')
-# be16 = prediction_error(bT16, dfBankTrain, 'y')
+# be1, pe1 = prediction(bT1, dfBankTest, 'y')
+# be2 = prediction(bT2, dfBankTest, 'y')
+# be3 = prediction(bT3, dfBankTest, 'y')
+# be4, pe4 = prediction(bT4, dfBankTest, 'y')
+# be5, pe5 = prediction(bT5, dfBankTest, 'y')
+# be6, pe6 = prediction(bT6, dfBankTest, 'y')
+# be7, pe7 = prediction(bT7, dfBankTest, 'y')
+# be8, pe8 = prediction(bT8, dfBankTest, 'y')
+# be9 = prediction(bT9, dfBankTest, 'y')
+# be10 = prediction(bT10, dfBankTest, 'y')
+# be11 = prediction(bT11, dfBankTest, 'y')
+# be12 = prediction(bT12, dfBankTest, 'y')
+# be13 = prediction(bT13, dfBankTest, 'y')
+# be14 = prediction(bT14, dfBankTest, 'y')
+# be15 = prediction(bT15, dfBankTest, 'y')
+# be16, pe16 = prediction(bT16, dfBankTest, 'y')
 
 # beAvg = (be1 + be2 + be3 + be4 + be5 + be6 + be7 + be8 + be9 + be10 + be11 + be12 + be13 + be14 + be15 + be16)/16
 # print(beAvg)
-#print("It worked!")
-
 
         
     
