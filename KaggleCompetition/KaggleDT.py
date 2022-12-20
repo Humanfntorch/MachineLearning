bd3e1299f511e99fb0a99fab07447e67fb894ff4
Jake <57159859+Humanfntorch@users.noreply.github.com>
2022-12-19 22:26:36 -0700

    Add files via upload

---
+++ KaggleCompetition/KaggleDT.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 19:23:52 2022
+
+@author: Jacob Rogers
+
+This is for the Kaggle competition for CS6350.
+
+The task is to predict whether an individual has an income > $50k
+based on surveyed data.
+"""
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import make_pipeline
+import seaborn as sn
+import matplotlib.pyplot as plt
+import csv
+
+
+# Replaces all instances of '?' in a column with one of the top 3 majority choices (excluding '?') in that column. 
+def complete_missing_value(df):
+    for col in df.columns:
+        if(len(df[col][df[col] == '?']) > 0):
+            mstCmnLbl = df[col].value_counts().index[:4]
+            newVal = np.random.choice(mstCmnLbl)
+            if(newVal == '?'):
+                mstCmnLbl = mstCmnLbl.drop("?")
+                newVal = np.random.choice(mstCmnLbl)
+            df.loc[df[col] == "?", col] = newVal
+
+
+label_name = 'income>50K'
+
+dfTrain = pd.read_csv('train_final.csv', header = 0, na_values = "?")
+dfTest = pd.read_csv('test_final.csv', header = 0, na_values = "?")
+
+
+# determine number of NAs in each df
+trainNA = len(dfTrain[dfTrain.isna().any(axis = 1)]) # 1848
+testNA = len(dfTest[dfTest.isna().any(axis = 1)]) # 1772
+fracTrainNA = trainNA / len(dfTrain) # ~ 7.4%
+fracTestNA = testNA / len(dfTest) # 0~ 7.4%
+
+# Drop NA in both dfs and call them xtr, ytr => train and xt => test
+corrNA = dfTrain.dropna() # Used to test correlations
+dfTrain = dfTrain.dropna()
+
+# sanity check: Ensure dropped NA dfs have expected sizes
+# assert len(xtr) + trainNA == len(dfTrain)
+# assert len(ytr) + trainNA == len(dfTrain)
+
+
+# # Determine percentages of yes/no labels
+# lessThan = len(ytr[ytr == 0]) / len(ytr) # ~ 75.1%
+# greaterThan = len(ytr[ytr == 1]) / len(ytr) # ~ 24.9%
+
+corrMat = corrNA.corr()
+# The category 'fnlwgt' produces a negative correlation
+
+# print corrMat
+sn.heatmap(corrMat, annot = True)
+plt.show()
+# sn.heatmap(corrMat2, annot = True)
+# plt.show()
+
+
+# drop 'fnlwgt' due to no corr
+# dfTrain = dfTrain.drop(columns = ['fnlwgt'])
+# dfTest = dfTest.drop(columns = ['fnlwgt'])
+# xtr = xtr.drop(columns = ['fnlwgt'])
+# ytr = dfTrain.dropna().drop(columns = ['fnlwgt'])
+# ytr = ytr[label_name]
+# # Sanity check. new values should be the same
+# assert len(xtr) == len(ytr)
+
+# Determine numerical atts and categorical atts
+numericalAtts = dfTrain.select_dtypes(include = ['number']).columns
+categoricalAtts = dfTrain.select_dtypes(include = object).columns
+# assert (len(numericalAtts) + len(categoricalAtts)) == len(xtr.columns)
+
+
+
+# encode categorical type values
+# enc = OneHotEncoder(handle_unknown='ignore')
+# testone = enc.fit(xtr['workclass'])
+
+y = dfTrain[label_name].values
+
+# encoding, which bins categorical types in bins [0, n] for ez numerical manipulation
+features_final = pd.get_dummies(dfTrain)
+for col in dfTrain.drop(columns=[label_name]).columns:
+  if col not in numericalAtts:
+    temp={}
+    for i in range(len(dfTrain[col].unique())):
+      temp[dfTrain[col].unique()[i]]=i
+    work = dfTrain[col].map(temp)
+    dfTrain[col]=work
+    
+features_final = pd.get_dummies(dfTest)
+for col in dfTest.columns:
+  if col not in numericalAtts:
+    temp={}
+    for i in range(len(dfTest[col].unique())):
+      temp[dfTest[col].unique()[i]]=i
+    work = dfTest[col].map(temp)
+    dfTest[col]=work
+    
+
+# Scale numerical data for appropriate range: STANDARD
+standard_scaler = StandardScaler()
+scaler = standard_scaler.fit(dfTrain[numericalAtts])
+dfTrain[numericalAtts] = scaler.transform(dfTrain[numericalAtts])
+scaler = standard_scaler.fit(dfTest[numericalAtts.drop(labels = label_name)])
+dfTest[numericalAtts.drop(labels = label_name)] = scaler.transform(dfTest[numericalAtts.drop(labels = label_name)])
+
+# Scale numerical data for appropriate range: MINMAX
+# min_max = MinMaxScaler()
+# scaler = min_max.fit(dfTrain[numericalAtts])
+# dfTrain[numericalAtts] = scaler.transform(dfTrain[numericalAtts])
+# scaler = min_max.fit(dfTest[numericalAtts.drop(labels = label_name)])
+# dfTest[numericalAtts.drop(labels = label_name)] = scaler.transform(dfTest[numericalAtts.drop(labels = label_name)])
+
+
+x = dfTrain.drop(columns = [label_name]).values
+dfTest = dfTest.drop(columns = ["ID"])
+xTest = dfTest.values
+
+
+
+### DECISION TREE CLASSIFIER
+
+# tree = DecisionTreeClassifier(criterion='entropy',
+#     min_samples_split=8,max_depth=10)
+
+# dt = tree.fit(dfTrain.drop(columns=[LBL]), trainLbls)
+
+# idArr = dfTest["ID"]
+# initPredict = dt.predict(dfTest.drop(columns = ["ID"]))
+
+### END DECISION TREE CLASSIFIER
+
+### RANDOM FOREST REGRESSOR MODEL
+
+# from sklearn.ensemble import RandomForestRegressor
+# rf = RandomForestRegressor(n_estimators = 300)
+# rfTree = rf.fit(x, y)
+# y_pred = rfTree.predict(x)
+# y_pred[y_pred > 0.5] = 1
+# y_pred[y_pred <= 0.5] = 0
+# acc = sum(y_pred == y)/len(y)
+# print(acc)
+
+# y_pred = rfTree.predict(xTest)
+
+# idPredictionList = list()
+# for i in range(len(y_pred)):
+#     idPredictionList.append([i + 1, y_pred[i]])
+# csvHeader = ["ID", "Prediction"]
+# with open("initPrediction9RF.csv", 'w', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(csvHeader)
+#     writer.writerows(idPredictionList)
+
+
+
+# idArr = dfTest["ID"]
+# initPredict = rfTree.predict(dfTest.drop(columns = ["ID"]))
+
+# idPredictionList = list()
+# for i in idArr:
+#     idPredictionList.append([i + 1, initPredict[i]])
+
+# csvHeader = ["ID", "Prediction"]
+# # write values to csv. Use newline = '' to prevent csv writing linebreaks automatically
+# with open("initPrediction5RF.csv", 'w', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(csvHeader)
+#     writer.writerows(idPredictionList)
+
+### END RANDOM FOREST REGRESSION MODEL
+
+
+
+### SGD CLASSIFIER
+
+# from sklearn.linear_model import SGDClassifier
+# clf = SGDClassifier(loss="hinge", penalty="l2", max_iter = 1000)
+# clf.fit(x, y)
+
+# y_pred = clf.decision_function(x)
+# y_pred[y_pred > 0.5] = 1
+# y_pred[y_pred <= 0.5] = 0
+
+# acc = sum(y_pred == y)/(len(y))
+# print("SGD accuracy: ", acc)
+
+# y_pred = clf.decision_function(xTest)
+# y_pred[y_pred > 0.5] = 1
+# y_pred[y_pred <= 0.5] = 0
+# idPredictionList = list()
+# for i in range(len(y_pred)):
+#     idPredictionList.append([i + 1, y_pred[i]])
+# csvHeader = ["ID", "Prediction"]
+# with open("initPrediction2SGD.csv", 'w', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(csvHeader)
+#     writer.writerows(idPredictionList)
+
+
+### SGD REGRESSOR
+# from sklearn.linear_model import SGDRegressor
+# clf = SGDRegressor(loss="squared_error", penalty="l2", max_iter = 1000)
+# clf.fit(x, y)
+
+# y_pred = clf.predict(x)
+# y_pred[y_pred > 0.5] = 1
+# y_pred[y_pred <= 0.5] = 0
+
+# acc = sum(y_pred == y)/(len(y))
+# print("SGD accuracy: ", acc)
+# y_pred = clf.predict(xTest)
+# idPredictionList = list()
+# for i in range(len(y_pred)):
+#     idPredictionList.append([i + 1, y_pred[i]])
+# csvHeader = ["ID", "Prediction"]
+# with open("initPredictionMinMaxSGD4.csv", 'w', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(csvHeader)
+#     writer.writerows(idPredictionList)
+
+
+
+### SVM
+# from sklearn.svm import SVR
+# clf = SVR(kernel='rbf')
+# clf.fit(x, y)
+# y_pred = clf.predict(x)
+# y_pred[y_pred > 0.5] = 1
+# y_pred[y_pred <= 0.5] = 0
+# acc = sum(y == y_pred)/len(y)
+# print("rbf acc: ", acc)
+
+# y_pred = clf.predict(xTest)
+# idPredictionList = list()
+# for i in range(len(y_pred)):
+#     idPredictionList.append([i + 1, y_pred[i]])
+# csvHeader = ["ID", "Prediction"]
+# with open("initPredictionMinMaxSVMRBF3.csv", 'w', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(csvHeader)
+#     writer.writerows(idPredictionList)
+
+
+import NN
+# reshape x and xTest to fit neural network
+x = x.reshape(x.shape[0], 1, x.shape[1])
+xTest = xTest.reshape(xTest.shape[0], 1, xTest.shape[1])
+nn = NN.neural_network(1)
+nn.insert_layer(NN.layer((14, 30), False))
+nn.insert_layer(NN.layer((), True))
+# nn.insert_layer(NN.layer((30, 30), False))
+# nn.insert_layer(NN.layer((), True))
+# nn.insert_layer(NN.layer((30, 30), False))
+nn.insert_layer(NN.layer((), True))
+nn.insert_layer(NN.layer((30, 1), False))
+nn.insert_layer(NN.layer((), True))
+epochs = 50
+nn.train(x, y, epochs)
+y_pred = nn.prediction(x, y)
+y_pred = np.asarray(y_pred)
+y_pred = y_pred.reshape(y_pred.shape[0])
+# y_pred[y_pred > 0.5] = 1
+# y_pred[y_pred <= 0.5] = 0
+# acc = sum(y == y_pred)/len(y)
+# print(acc)
+# y_pred = nn.prediction(xTest, y)
+# y_pred = np.asarray(y_pred)
+# y_pred = y_pred.reshape(y_pred.shape[0])
+# idPredictionList = list()
+# for i in range(len(y_pred)):
+#     idPredictionList.append([i + 1, y_pred[i]])
+# csvHeader = ["ID", "Prediction"]
+# with open("initPredictionNN2.csv", 'w', newline = '') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(csvHeader)
+#     writer.writerows(idPredictionList)    
+# plot_epochs = np.arange(1, epochs + 1, 1)
+# figure, ax = plt.subplots(figsize=(10, 8))
+# line1, = ax.plot(plot_epochs, nn.losses)
+# plt.xlabel("Epoch")
+# plt.ylabel("Loss")
+# plt.title("Loss v Epoch")
+        
+
---
+++ KaggleCompetition/NN.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jacob Rogers
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Initializes a node in the network graph. 
+    # Each node has at least one incoming edge (base value or calculated)
+    # Each node should have at least one outgoing edge
+    # Each node has the ability to use activation function + derivative
+class node:
+    def __init__(self):
+        self.in_edge = None
+        self.out_edge = None
+        
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def p_sigmoid(self, x):
+        return (self.sigmoid(x)*(1 - self.sigmoid(x)))
+
+# Each layer is composed of a defined number of nodes, each with:
+    # A set of weights where w_ij belongs to node i and connects to node j
+    # The ability to distinguish layer type (excluding output layer) [hidden/non-hidden]
+    # The ability to propagate forward. [hidden/non-hidden]
+    # The ability to propagate backwards. [hidden/non-hidden]
+class layer(node):
+    # Weights represents how weights are initialized:
+        # "rand" weights are initialized randomly
+        # "zero" weights are initialized with 0
+    # dims defines the size of input nodes/output nodes as (input, output)
+    # width defines the width of the gaussian distribution to draw from. Default 5
+    def __init__(self, dims, isHidden, weights = 'rand'):
+        self.isHidden = isHidden
+        if(not isHidden):
+            if(weights == 'rand'):
+                self.weights = np.random.normal(size = (dims[0], dims[1]))
+                self.bias = np.random.normal(size = (1, dims[1]))
+            elif(weights == 'zero'):
+                self.weights = np.zeros([dims[0], dims[1]])
+                self.bias = np.zeros([1, dims[1]])
+            elif(weights == 'ones'):
+                self.weights = np.ones([dims[0], dims[1]])
+                self.bias = 0
+            
+    def forward_propagate(self, x):
+        if(self.isHidden):
+            self.in_edge = x
+            self.out_edge = self.sigmoid(self.in_edge)
+            return(self.out_edge)
+        else:
+            self.in_edge = x
+            self.out_edge = np.dot(self.in_edge, self.weights) + self.bias
+            return self.out_edge
+    
+    def back_propagate(self, gradient, learn_rate):
+        if(self.isHidden):
+            return(self.p_sigmoid(self.in_edge)*gradient)
+            # return(self.in_edge*(1 - self.in_edge)*gradient)
+            #return(self.out_edge*(1 - self.out_edge)*gradient)
+        else:
+            # sum of product of weights with current gradient = back propagated gradient
+            updated_grad = np.dot(gradient, self.weights.T)
+            # update weight based on running gradient cost
+            self.weights = self.weights - learn_rate*np.dot(self.in_edge.T, gradient)
+            # update bias based on running gradient cost
+            self.bias = self.bias - learn_rate*gradient
+            return updated_grad
+    
+class neural_network:
+   def __init__(self, init_learn_rate):
+       self.learning_rate = init_learn_rate
+       self.layers = []
+       self.losses = []
+       self.y_preds = []
+       self.accuracy = None
+       
+   def insert_layer(self, layer):
+       self.layers.append(layer)
+       
+   def _loss(self, y, y_t):
+       return(1/2*((y - y_t)**2).mean())
+       
+   def del_loss(self, y, y_t):
+       return((y_t - y)/y.size)
+   
+   def train(self, x, y, epochs):
+       self.losses = []
+       
+       # Scheduling for learning rate
+       for i in range(epochs):
+           d = (i + 1)**10
+           loss = 0
+           self.learning_rate = (self.learning_rate)/(1 + (self.learning_rate*i)/d)
+           for j in range(len(x)):
+               # initial input from first layer
+               out_edge = x[j]
+               for layer in self.layers:
+                   out_edge = layer.forward_propagate(out_edge)
+               # Propagated to output layer. Calc loss and del_loss.   
+               loss += self._loss(y[j], out_edge)
+               dloss = self.del_loss(y[j], out_edge)
+               # Back propagate the cached values with the partial loss
+               for k in range(len(self.layers) - 1, -1, -1):
+                    dloss = self.layers[k].back_propagate(dloss, self.learning_rate)
+           self.losses.append(loss/len(x))
+        
+          
+           
+   def prediction(self, x, y, threshold = 0.5):
+       # reset list of weights
+       self.y_preds = []
+       self.accuracy = 0
+       # Perform forward propagation on trained network using x as input
+       for i in range(len(x)):
+           out_edge = x[i]
+           for layer in self.layers:
+               out_edge = layer.forward_propagate(out_edge)
+           self.y_preds.append(out_edge)
+       self.accuracy = sum(y == self.y_preds)/len(y) 
+       return self.y_preds
+
+
+
+# dfTest = pd.read_csv('test.csv')
+# dfTrain = pd.read_csv('train.csv')
+# # Real label name is genuine/forged. I'm changing it to label for easier use
+# cols = ["variance of Wavelet Transformed image", "skewness of Wavelet Transformed image", "curtosis of Wavelet Transformed image", "entropy of image", "label"]
+# dfTest.columns = cols
+# dfTrain.columns = cols
+# # convert all zero labels to -1
+# dfTrain['label'] = dfTrain['label'].replace(0, -1)
+# dfTest['label'] = dfTest['label'].replace(0, -1);
+
+# # Store x/y as train test variables
+# yTest = dfTest['label'].values
+# yTrain = dfTrain['label'].values
+# xTest = dfTest.drop(columns = ['label']).values
+# xTrain = dfTrain.drop(columns = ['label']).values
+# xTrain = xTrain.reshape(xTrain.shape[0], 1, xTrain.shape[1])
+# xTest = xTest.reshape(xTest.shape[0], 1, xTest.shape[1])
+
+
+# width = [5, 10, 25, 50, 100]
+# epochs = 100
+# threshold = 0.5
+# weight_type = 'zero'
+# for i in range(len(width)):
+#     nn = neural_network(1)
+#     # Layer 1
+#     nn.insert_layer(layer((4, width[i]), False, weight_type))
+#     nn.insert_layer(layer((), True))
+#     # Layer 2
+#     nn.insert_layer(layer((width[i], 1), False, weight_type))
+#     nn.insert_layer(layer((), True))
+#     # Layer 3
+
+#     nn.train(xTrain, yTrain, epochs)
+#     y = nn.prediction(xTrain, yTrain, threshold)
+
+#     print("Node width = {width}, Training Error = {err}".format(width = width[i], err = 1 - nn.accuracy))
+#     y = nn.prediction(xTest, yTest, threshold)
+#     print("Node width = {width}, Testing Error = {err}".format(width = width[i], err = 1 - nn.accuracy))
+
+#     plot_epochs = np.arange(1, epochs + 1, 1)
+#     figure, ax = plt.subplots(figsize=(10, 8))
+#     line1, = ax.plot(plot_epochs, nn.losses)
+#     plt.xlabel("Epoch")
+#     plt.ylabel("Loss")
+#     plt.title("Loss v Epoch")
